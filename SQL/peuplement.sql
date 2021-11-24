fe4d1a43606031305d1264baaa03108b991b8612
HAMDOUNI Ghofrane <Ghofrane.Hamdouni@bordeaux-inp.fr>
2021-11-24 15:47:42 +0100

    DONNEES / VELOS

---
+++ SQL/peuplement.sql
@@ -0,0 +1,151 @@
+--VELO
+insert into VELO values (1,'O2Feel',20000,'01-JAN-16',50,1)
+insert into VELO values (2,'Maa Bikes',300000,'01-JAN-16',60,12)
+insert into VELO values (3,'EXS',40000,'01-JAN-16',70,6)
+insert into VELO values (4,'EXS',30000,'01-JAN-16',80,6)
+insert into VELO values (5,'EXS',20000,'01-FAB-17',90,6)
+insert into VELO values (6,'O2Feel',20000,'01-FAB-17',100,6)
+insert into VELO values (7,'O2Feel',20000,'01-JAN-16',50,6)
+insert into VELO values (8,'O2Feel',8000,'01-FAB-17',50,1)
+insert into VELO values (9,'O2Feel',12000,'01-JAN-18',50,1)
+insert into VELO values (10,'CyclO2',9000,'01-JAN-18',50,1)
+--------------------------------------------------------------
+insert into VELO values (11,'O2Feel',80000,'01-JAN-18',50,2)
+insert into VELO values (12,'O2Feel',60000,'01-JAN-18',80,2)
+insert into VELO values (13,'O2Feel',63000,'01-JAN-16',40,2)
+insert into VELO values (14,'O2Feel',20000,'01-JAN-16',50,2)
+insert into VELO values (15,'CyclO2',80000,'01-FAB-17',50,5)
+insert into VELO values (16,'O2Feel',4000,'01-JAN-20',40,5)
+insert into VELO values (17,'Eovolt',4000,'01-JAN-20',50,2)
+insert into VELO values (18,'CyclO2',100000,'01-JAN-19',50,3)
+insert into VELO values (19,'Eovolt',80000,'01-JAN-19',75,1)
+insert into VELO values (20,'Eovolt',40000,'01-JAN-16',75,5)
+
+-------------------------------------------------------------- 
+insert into VELO values (21,'O2Feel',200000,'01-FAB-17',50,3)
+insert into VELO values (22,'Eovolt',25000,'01-FAB-17',80,3)
+insert into VELO values (23,'Eovolt',70000,'01-JAN-16',40,5)
+insert into VELO values (24,'Eovolt',34800,'01-JAN-19',50,5)
+insert into VELO values (25,'CyclO2',7890,'01-JAN-20',100,3)
+insert into VELO values (26,'O2Feel',20840,'01-JAN-20',40,3)
+insert into VELO values (27,'Eovolt',7894,'01-JAN-20',50,4)
+insert into VELO values (28,'CyclO2',203610,'01-JAN-16',50,4)
+insert into VELO values (29,'Eovolt',789450,'01-JAN-16',75,5)
+insert into VELO values (30,'Eovolt',14466,'01-JAN-18',75,3)
+insert into VELO values (31,'Eovolt',145480,'01-JAN-16',65,4)  
+
+---------------------------------------------------------- 
+
+insert into VELO values (32,'Maa Bikes',30569,'01-JAN-16',60,7)
+insert into VELO values (33,'EXS',401254,'01-JAN-16',70,6)
+insert into VELO values (34,'EXS',78163,'01-JAN-16',80,5)
+insert into VELO values (35,'EXS',78940,'01-JAN-16',90,3)
+insert into VELO values (36,'O2Feel',23648,'01-JAN-18',100,4)
+insert into VELO values (37,'O2Feel',164600,'01-JAN-16',50,7)
+insert into VELO values (38,'O2Feel',200,'01-JAN-21',50,1)
+insert into VELO values (39,'O2Feel',20846,'01-JAN-20',50,1)
+insert into VELO values (40,'CyclO2',2045,'01-JAN-21',50,7)
+
+
+----------------------------------------------------------------- 
+
+
+insert into VELO values (41,'Eovolt',245050,'01-JAN-16',50,7)
+insert into VELO values (42,'O2Feel',7980,'01-JAN-20',80,2)
+insert into VELO values (43,'O2Feel',145340,'01-JAN-16',40,7)
+insert into VELO values (44,'O2Feel',7894050,'01-FAB-18',50,8)
+insert into VELO values (45,'CyclO2',45691,'01-JAN-17',50,9)
+insert into VELO values (46,'O2Feel',20463,'01-JAN-17',40,12)
+insert into VELO values (47,'Eovolt',21450,'01-JAN-17',50,8)
+insert into VELO values (48,'CyclO2',27890,'01-JAN-17',50,8)
+insert into VELO values (49,'Eovolt',279860,'01-JAN-16',75,8)
+insert into VELO values (50,'Eovolt',215680,'01-JAN-16',75,8)
+insert into VELO values (51,'Eovolt',248720,'01-JAN-16',65,10) 
+
+
+----------------------------------------------------------
+insert into VELO values (52,'O2Feel',24675,'01-JAN-19',80,2)
+insert into VELO values (53,'O2Feel',149710,'01-JAN-18',40,9)
+insert into VELO values (54,'O2Feel',459720,'01-JAN-16',50,9)
+insert into VELO values (55,'CyclO2',489270,'01-JAN-16',50,9)
+insert into VELO values (56,'O2Feel',48220,'01-JAN-19',40,12)
+insert into VELO values (57,'Eovolt',2000,'01-JAN-20',50,12)
+insert into VELO values (58,'CyclO2',7820,'01-JAN-20',50,11)
+insert into VELO values (59,'Eovolt',10000,'01-JAN-20',75,11)
+insert into VELO values (60,'Eovolt',887,'01-JAN-21',75,9)
+insert into VELO values (61,'Eovolt',201,'01-JAN-21',65,10) 
+
+
+
+------------------------------------------------------------ 
+
+
+insert into VELO values (62,'Maa Bikes',300000,'01-JAN-16',60,11)
+insert into VELO values (63,'EXS',40000,'01-JAN-16',75,12)
+insert into VELO values (64,'EXS',78940,'01-FAB-17',80,12)
+insert into VELO values (65,'EXS',125400,'01-JAN-16',90,11)
+insert into VELO values (66,'O2Feel',789420,'01-JAN-16',100,10)
+insert into VELO values (67,'Eovolt',14580,'01-JAN-19',50,8)
+insert into VELO values (68,'Eovolt',78942,'01-JAN-19',50,10)
+insert into VELO values (69,'Eovolt',89420,'01-FAB-01',50,11)
+insert into VELO values (70,'CyclO2',87452,'01-FAB-01',50,12) 
+
+--------------------------------------------------------------------
+
+
+
+insert into VELO values (72,'Maa Bikes',379470,'01-JAN-16',60,11)
+insert into VELO values (73,'EXS',89705,'01-JAN-16',75,12)
+insert into VELO values (74,'EXS',124580,'01-JAN-18',80,12)
+insert into VELO values (75,'EXS',48960,'01-JAN-18',90,11)
+insert into VELO values (76,'O2Feel',248560,'01-JAN-18',100,9)
+insert into VELO values (77,'Eovolt',247850,'01-JAN-18',50,9)
+insert into VELO values (78,'Eovolt',212450,'01-JAN-18',50,8)
+insert into VELO values (79,'Eovolt',27840,'01-JAN-18',50,8)
+insert into VELO values (80,'CyclO2',258770,'01-JAN-18',50,7)  
+
+-------------------------------------------------------------
+
+
+insert into VELO values (82,'O2Feel',20,'01-JAN-21',80,2)
+insert into VELO values (83,'O2Feel',20,'01-JAN-21',40,10)
+insert into VELO values (84,'O2Feel',100,'01-JAN-21',50,11)
+insert into VELO values (85,'CyclO2',10,'01-JAN-21',50,9)
+insert into VELO values (86,'O2Feel',800,'01-JAN-21',40,10)
+insert into VELO values (87,'Eovolt',1050,'01-JAN-21',50,12)
+insert into VELO values (88,'CyclO2',4000,'01-JAN-21',50,10)
+insert into VELO values (89,'Eovolt',790,'01-JAN-21',75,10)
+insert into VELO values (90,'Eovolt',200,'01-JAN-21',75,7)
+insert into VELO values (91,'Eovolt',8,'01-JAN-21',65,8) 
+
+---------------------------------------------------------------------- 
+
+
+insert into VELO values (92,'Maa Bikes',18030,'01-JAN-20',60,10)
+insert into VELO values (93,'EXS',7050,'01-JAN-20',75,8)
+insert into VELO values (94,'Maa Bikes',2040,'01-JAN-20',80,6)
+insert into VELO values (95,'Maa Bikes',1500,'01-JAN-20',90,5)
+insert into VELO values (96,'O2Feel',700,'01-JAN-21',100,11)
+insert into VELO values (97,'Eovolt',800,'01-JAN-11',50,9)
+insert into VELO values (98,'Maa Bikes',2770,'01-JAN-21',50,7)
+insert into VELO values (99,'Eovolt',48000,'01-JAN-20',50,4)
+insert into VELO values (100,'CyclO2',21850,'01-JAN-20',50,6)   
+
+
+
+insert into VELO values (101,'EXS',40000,'01-FAB-17',90,3)
+insert into VELO values (102,'CyclO2',40000,'01-FAB-17',90,4)
+insert into VELO values (103,'CyclO2',408405,'01-FAB-17',90,4) 
+insert into VELO values (104,'EXS',70000,'01-FAB-17',90,3)
+insert into VELO values (105,'EXS',80000,'01-FAB-17',90,3)
+insert into VELO values (106,'Maa Bikes',9700,'01-JAN-21',90,4)
+insert into VELO values (107,'Maa Bikes',6089,'01-JAN-21',90,1)
+insert into VELO values (108,'CyclO2',700,'01-JAN-21',90,1) 
+insert into VELO values (109,'Maa Bikes',885,'01-JAN-21',90,11)
+insert into VELO values (110,'Maa Bikes',647805,'01-FAB-17',90,7)
+insert into VELO values (111,'O2Feel',289702,'01-FAB-17',60.5,9)
+insert into VELO values (112,'Eovolt',87892,'01-FAB-17',65,10) 
+insert into VELO values (113,'Eovolt',328246,'01-JAN-19',75,7)
+insert into VELO values (114,'Eovolt',6596472,'01-JAN-19',75,1)
+
+
