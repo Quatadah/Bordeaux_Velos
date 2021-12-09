064c695cfeecb767c7971a19837f51d324089b6a
Sadiq Doha <Doha.Sadiq@bordeaux-inp.fr>
2021-12-09 18:26:59 +0100

    rectification admin.ph

--- Application/admin/admin.php
+++ Application/admin/admin.php
@@ -17,11 +17,11 @@
             </div>
             <p id="parag">L'interface administrateur permet la gestion de base de données
                 des vélos de Bordeaux Métropole. Vous avez la possibilité de consulter, 
-                mettre à jour les lables de votre base ainsi que de voir les statistiques.
+                mettre à jour les tables de votre base ainsi que de voir les statistiques.
             </p>
             <ul id="choice-list">
                 <li class="choice-ele"><a href="consultation.php">Consultation</a></li>
-                <li class="choice-ele"><a href="misajour.php">Mis à jour</a></li>
+                <li class="choice-ele"><a href="misajour.php">Mises à jour</a></li>
                 <li class="choice-ele"><a href="stats.php">Statistiques</a></li>
             </ul>
             <div>
