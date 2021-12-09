026502b6a7c6241e877f6f55e16217b8efda5f9e
NASDAMI Quatadah <Quatadah.Nasdami@bordeaux-inp.fr>
2021-12-09 19:33:34 +0100

    rectification mode.php

--- Application/mode.php
+++ Application/mode.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/mode.css">
-    <link rel="stylesheet" href="style/general.css">
+    
     <link rel="stylesheet" href="style/app.css">
     <title>Mode</title>
 </head>
@@ -16,12 +16,9 @@
             <div id="choix-mode">
                 <h1>Choix du mode</h1>
             </div>
-            <div id="parag">
-                <p>
-                    Choississez le mode selon lequel vous voulez utiliser l'application </p>
-            </div>  
+
             <ul class="mode-list">
-                <li class="choix"><a href="index.php">Utilisateur</a></li>
+                <li class="choix" id="mrg" ><a href="index.php">Utilisateur</a></li>
                 <li class="choix"><a href="admin/admin.php">Administrateur</a></li>
             </ul>
         </div>
